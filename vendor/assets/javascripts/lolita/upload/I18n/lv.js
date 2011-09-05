aed47e7bc7c8725d58305c4040d4ff5a61e16432
Arturs Meisters <arturs@ithouse.lv>
2011-09-05 14:07:10 +0300

    files

---
+++ vendor/assets/javascripts/lolita/upload/I18n/lv.js
@@ -0,0 +1,22 @@
+// Latvian
+plupload.addI18n({
+    'Select files' : 'Izvēlieties failu',
+    'Add files to the upload queue and click the start button.' : 'Pievienojiet failu augšupielādes rindai un nospiediet pogu "Sākt augšupielādi".',
+    'Filename' : 'Faila nosaukums',
+    'Status' : 'Statuss',
+    'Size' : 'Izmērs',
+    'Add Files' : 'Pievienot failus',
+    'Start Upload': "Sākt augšupielādi",
+    'Stop current upload' : 'Apturēt augšupielādi',
+    'Start uploading queue' : 'Sākt augšupielādēt rindu',
+    'Uploaded %d/%d files': 'Augšupielādēti %d no %d failiem',
+    'N/A' : 'N/A',
+    'Drag files here.' : 'Pārvilkt failus šeit.',
+    'File extension error.': 'Faila paplašinājuma kļūda.',
+    'File size error.': 'Faila izmēra kļūda.',
+    'Init error.': 'Inicializācijas kļūda.',
+    'HTTP Error.': 'HTTP kļūda.',
+    'Security error.': 'Drošības kļūda.',
+    'Generic error.': 'Vispārīga kļūda.',
+    'IO error.': 'Ievades-izvades kļūda.'
+});
