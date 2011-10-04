15ab924c03d625b0ebeef900c400ea9a71fb19e3
Arturs Meisters <arturs@ithouse.lv>
2011-10-05 00:46:54 +0300

    fixed_gallery updated

--- lib/lolita-file-upload/module.rb
+++ lib/lolita-file-upload/module.rb
@@ -5,6 +5,9 @@ Lolita.add_module Lolita::FileUpload, :nested=>true,:route=>lambda{|resource|
   end
 }
 # append fixed gallery
-Lolita::Hooks.component(:"lolita/configuration/tab/default/display").before do
-  render_component(:"lolita/configuration/tab/files", :fixed_gallery) if resource_class.lolita.tabs.by_type(:files)
+Lolita::Hooks.component(:"/lolita/configuration/tabs/display").before do
+  resource_class = self.send(:resource_class)
+  if resource_class.lolita.tabs.by_type(:files)
+    self.render_component(:"/lolita/configuration/tab/files", :fixed_gallery) 
+  end
 end
