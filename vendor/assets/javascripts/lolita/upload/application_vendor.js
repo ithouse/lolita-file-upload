8d732cec1121a2a3a7bc0ad3fb2b7753d2678116
Arturs Meisters <arturs@ithouse.lv>
2012-09-03 13:12:37 +0300

    stupid alert removed

--- vendor/assets/javascripts/lolita/upload/application_vendor.js
+++ vendor/assets/javascripts/lolita/upload/application_vendor.js
@@ -2,7 +2,6 @@
 //= require lolita/upload/plupload.full
 //= require lolita/upload/jquery.ui.plupload
 
-alert(1)
 $(document).ready(function(){
   $(document).on("click", ".file-editor a.save", function(event) {
     $(this).closest("form").submit();
