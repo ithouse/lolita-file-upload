412bd88373754921a114645aa5d55288d7d1fd7b
Māris Cīlītis <maris@ithouse.lv>
2014-03-18 15:26:44 +0200

    strong param fix;

--- lib/lolita-file-upload/rails/file_upload_routes.rb
+++ lib/lolita-file-upload/rails/file_upload_routes.rb
@@ -6,7 +6,7 @@ module ActionDispatch::Routing
       # /lolita/articles/1/pictures/5
       resources mapping.plural, :module=>mapping.module, :only=>:none do
         collection do
-          match "/:association/create", :to => "#{controllers[:uploads]}#create", :as => "create_upload", :method => :post
+          match "/:association/create" => "#{controllers[:uploads]}#create", :as => "create_upload", :via => :post
         end
         resources :uploads, :controller => controllers[:uploads], :path => "/:association"
       end
