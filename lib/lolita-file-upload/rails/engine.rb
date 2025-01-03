a0ccc4a757f7bea793a0002a542f1f57355ae76e
Aivils Štoss <aivils@ithouse.lv>
2025-01-03 15:17:02 +0200

    tinymce 5 compatible

--- lib/lolita-file-upload/rails/engine.rb
+++ lib/lolita-file-upload/rails/engine.rb
@@ -2,6 +2,7 @@ module Lolita::FileUpload
   class Engine < ::Rails::Engine
     initializer 'precompile', group: :all do |app|
       app.config.assets.precompile += %w(tinymce/plugins/lolita_gallery/plugin.js)
+      app.config.assets.precompile << "lolita-file-upload-manifest.js" # Sprockets 4 manifest
     end
   end
 end
